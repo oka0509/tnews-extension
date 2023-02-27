f6a6e8fc37348678de07819367826a2c6535e119
oka0509 <76876781+oka0509@users.noreply.github.com>
2023-02-27 21:39:09 +0900

    フォームを変更

--- content.js
+++ content.js
@@ -1,14 +1,22 @@
-let prefecturesDiv = document.getElementById('baito');
-let prefecturesList = prefecturesDiv.children;
+// let prefecturesDiv = document.getElementById('baito');
+// let prefecturesList = prefecturesDiv.children;
 
-let prefecturesListLength = prefecturesList.length;
-console.log(prefecturesListLength);
+// let prefecturesListLength = prefecturesList.length;
+// console.log(prefecturesListLength);
 
-for(let i = 0; i< prefecturesListLength; i++) {
-  if(prefecturesList[i].tagName == 'SPAN' &&
-  prefecturesList[i].firstChild.textContent != '東京都' && 
-  prefecturesList[i].firstChild.textContent != '過去の求人' ) {
-    console.log(prefecturesList[i].firstChild.textContent);
-    prefecturesList[i].style.visibility = 'hidden';
-  }
-}
+// for(let i = 0; i< prefecturesListLength; i++) {
+//   if(prefecturesList[i].tagName == 'SPAN' &&
+//   prefecturesList[i].firstChild.textContent != '東京都' && 
+//   prefecturesList[i].firstChild.textContent != '過去の求人' ) {
+//     console.log(prefecturesList[i].firstChild.textContent);
+//     prefecturesList[i].style.visibility = 'hidden';
+//   }
+// }
+
+chrome.storage.local.set({'key': 'ネコ猫'}, function () {
+});
+
+chrome.storage.local.get("key", function (value) {
+  var value_data = value.key;
+  console.log(value_data);
+});
