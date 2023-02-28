2bba6eb50b7c7b43eee485133321fec9fa9659b7
oka0509 <76876781+oka0509@users.noreply.github.com>
2023-02-28 12:26:27 +0900

    追加前に削除しておく

--- popup.js
+++ popup.js
@@ -14,12 +14,22 @@ function getLocalStorage(key = null) {
   });
 }
 
+function clearLocalStorage() {
+  return new Promise( (resolve) => {
+    chrome.storage.local.clear(() => {
+      resolve();
+    })
+  });
+}
+
 let init = async (arr) => {
+  await clearLocalStorage();
   for(let i = 0; i< arr.length; i++) {
     await setLocalStorage({[arr[i].defaultValue]: true});
   }
-  let aaa = await getLocalStorage("北海道");
-  console.log(aaa);
+  // let aaa = await getLocalStorage("北海道");
+  // console.log(aaa);
+  //content.jsに対して変更処理を送る
 };
 submitButton.addEventListener("click", ()=> {
   let checkboxes = document.getElementsByName("prefecture");
