d20961bc73df447041e6e594f1ecd0afa9538a04
oka0509 <76876781+oka0509@users.noreply.github.com>
2023-02-28 21:54:18 +0900

    minor fix

--- content.js
+++ content.js
@@ -1,16 +1,13 @@
 chrome.storage.local.get(null, (items) => {
   let registeredPrefectures = Object.keys(items);
-  console.log(registeredPrefectures);
   let prefecturesDiv = document.getElementById('baito');
   let prefecturesList = prefecturesDiv.children;
 
   let prefecturesListLength = prefecturesList.length;
-  console.log(prefecturesListLength);
   for(let i = 0; i< prefecturesListLength; i++) {
     if(prefecturesList[i].tagName == 'SPAN' &&
     !registeredPrefectures.includes(prefecturesList[i].firstChild.textContent) && 
     prefecturesList[i].firstChild.textContent != '過去の求人' ) {
-      console.log(prefecturesList[i].firstChild.textContent);
       prefecturesList[i].style.visibility = 'hidden';
     }
   }
@@ -18,11 +15,9 @@ chrome.storage.local.get(null, (items) => {
 
 chrome.runtime.onMessage.addListener((request, sendResponse) => {
   let resText = "visibility changed";
-  console.log(request.message);
 
   chrome.storage.local.get(null, (items) => {
     let registeredPrefectures = Object.keys(items);
-    console.log(registeredPrefectures);
 
     let prefecturesDiv = document.getElementById('baito');
     let prefecturesList = prefecturesDiv.children;
@@ -31,7 +26,6 @@ chrome.runtime.onMessage.addListener((request, sendResponse) => {
       if(prefecturesList[i].tagName == 'SPAN' &&
       prefecturesList[i].firstChild.textContent != '過去の求人') {
         if(!registeredPrefectures.includes(prefecturesList[i].firstChild.textContent)) {
-          console.log(prefecturesList[i].firstChild.textContent);
           prefecturesList[i].style.visibility = 'hidden';
         }
         else {
--- popup.js
+++ popup.js
@@ -1,8 +1,5 @@
-let submitButton = document.getElementById("addPrefectures");
-
 chrome.storage.local.get(null, (items) => {
   let registeredPrefectures = Object.keys(items);
-  console.log(registeredPrefectures);
   let checkboxes = document.getElementsByName("prefecture");
   checkboxes.forEach((e) => {
     if(registeredPrefectures.includes(e.value)) {
@@ -33,7 +30,7 @@ function clearLocalStorage() {
   });
 }
 
-let init = async (arr) => {
+async function migrate(arr) {
   await clearLocalStorage();
   for(let i = 0; i< arr.length; i++) {
     await setLocalStorage({[arr[i].defaultValue]: true});
@@ -42,14 +39,14 @@ let init = async (arr) => {
   chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
     // 取得したタブid(tabs[0].id)を利用してsendMessageする
     chrome.tabs.sendMessage(tabs[0].id, {message: 'prefecturesRegistered'}, (res) => {
-      console.log(res);
     });
   });
 };
 
+let submitButton = document.getElementById("addPrefectures");
+
 submitButton.addEventListener("click", ()=> {
   let checkboxes = document.getElementsByName("prefecture");
   let checkedPrefectures = Array.from(checkboxes).filter(element => element.checked == true);
-  console.log(checkedPrefectures[0].defaultValue);
-  init(checkedPrefectures);
+  migrate(checkedPrefectures);
 });
